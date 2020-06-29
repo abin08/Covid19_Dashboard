79c16addf17a6963d434ce58ce80e173a4a5f76a
abin08 <abinkantony64@gmail.com>
2020-06-29 18:54:55 +0530

    minor changes

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,15 @@ from app import app
 from app import covid
 
 
-@app.route("/dashboard")
+@app.route("/")
 def dashboard():
     total_info = {}
     dataframe = covid.get_covid_data()
     
-    top_ten_confirmed = covid.top_ten_confirmed(dataframe).to_json()
-    top_ten_recovered = covid.top_ten_recovered(dataframe).to_json()
-    top_ten_deaths = covid.top_ten_deaths(dataframe).to_json()
-
+    top_ten_confirmed = covid.get_top10_confirmed(dataframe)
+    top_ten_recovered = covid.get_top10_recovered(dataframe)
+    top_ten_deaths = covid.get_top10_deaths(dataframe)
+    
     total_info['total_confirmed'] = int(covid.total_confirmed(dataframe))
     total_info['total_recovered'] = int(covid.total_recovered(dataframe))
     total_info['total_deaths'] = int(covid.total_deaths(dataframe))
@@ -28,13 +28,3 @@ def dashboard():
         top_ten_recovered=top_ten_recovered, 
         top_ten_deaths=top_ten_deaths
     )
-
-
-@app.route("/test")
-def test():
-    total_info = {}
-    dataframe = covid.get_covid_data()
-    
-    top_ten_confirmed = covid.top_ten_confirmed(dataframe).to_json()
-    
-    return top_ten_confirmed
