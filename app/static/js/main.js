0fc37e5e9dde35d42f91ff0872f3e4320fd09894
abin08 <abinkantony64@gmail.com>
2020-06-30 16:03:31 +0530

    changed color

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,7 @@
-const ACTIVE_COLOR = '#f77f00'
-const RECOVERED_COLOR = '#1d3557'
-const DEATHS_COLOR = '#d62828'
+const ACTIVE_COLOR = '#1d3557'
+const RECOVERED_COLOR = '#009900'
+const DEATHS_COLOR = '#ff0000'
+
+// const ACTIVE_COLOR = '#ffa600'
+// const RECOVERED_COLOR = '#003f5c'
+// const DEATHS_COLOR = '#ff0000'
